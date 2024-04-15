2de5933669024d0e316729e7af393ce419c7c2b5
Konov Aleksey <konov.alexey@gmail.com>
2024-04-15 10:09:28 +0300

    Create BirthdayCreateView by CBV, instead view.create

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'birthday'
 
 urlpatterns = [
-    path('', views.birthday, name='create'),
+    path('', views.BirthdayCreateView.as_view(), name='create'),
     path('<int:pk>/edit/', views.birthday, name='edit'),
     path('<int:pk>/delete/', views.delete_birthday, name='delete'),
     path('list/', views.BirthdatListView.as_view(), name='list'),
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,37 +1,45 @@
-from django.core.paginator import Paginator
-
 from django.shortcuts import get_object_or_404, redirect, render
 
-from django.views.generic import ListView
+from django.views.generic import CreateView, ListView
+
+from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
-def birthday(request, pk=None):
-    if pk is not None:
-        instance = get_object_or_404(Birthday, pk=pk)
-    else:
-        instance = None
-
-    form = BirthdayForm(
-        request.POST or None,
-        files=request.FILES or None,
-        instance=instance)
-    context = {
-        'form': form
-    }
-
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        context['birthday_countdown'] = birthday_countdown
-
+class BirthdayCreateView(CreateView):
+    model = Birthday
+    form_class = BirthdayForm
     template_name = 'birthday/birthday.html'
-    return render(request, template_name, context)
+    success_url = reverse_lazy('birthday:list')
+
+
+# Реализация без CBV
+# def birthday(request, pk=None):
+#     if pk is not None:
+#         instance = get_object_or_404(Birthday, pk=pk)
+#     else:
+#         instance = None
+
+#     form = BirthdayForm(
+#         request.POST or None,
+#         files=request.FILES or None,
+#         instance=instance)
+#     context = {
+#         'form': form
+#     }
+
+#     if form.is_valid():
+#         form.save()
+#         birthday_countdown = calculate_birthday_countdown(
+#             form.cleaned_data['birthday']
+#         )
+#         context['birthday_countdown'] = birthday_countdown
+
+#     template_name = 'birthday/birthday.html'
+#     return render(request, template_name, context)
 
 
 def delete_birthday(request, pk):
